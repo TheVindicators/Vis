580e6b57f68055cf0d8785a25d638a30e5bec61a
Christian Evans <cade9522@gmail.com>
2017-10-25 16:39:56 -0400

    added info box, fixed typo

--- app/static/js/Menubar.js
+++ app/static/js/Menubar.js
@@ -10,6 +10,7 @@ var Menubar = function ( editor ) {
 	container.add( new Menubar.File( editor ) );
 	container.add( new Menubar.Edit( editor ) );
 	container.add( new Menubar.Add( editor ) );
+	container.add( new Menubar.Help( editor ) );
 	container.add( new Menubar.Status( editor ) );
 
 	return container;
