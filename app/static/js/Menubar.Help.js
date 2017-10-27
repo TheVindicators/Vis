18af9252d4ebf60d1faf367f76f7574cb379b334
Christian Evans <cade9522@gmail.com>
2017-10-27 00:15:02 -0400

    less confusion, added links

--- app/static/js/Menubar.Help.js
+++ app/static/js/Menubar.Help.js
@@ -23,16 +23,16 @@ Menubar.Help = function ( editor ) {
 	option.setTextContent( 'Source code' );
 	option.onClick( function () {
 
-		window.open( 'https://github.com/mrdoob/three.js/tree/master/editor', '_blank' )
+		window.open( 'https://github.com/mrdoob/three.js/tree/master/editor', '_blank' );
 
 	} );
 	//options.add( option );
 
-	// About
+	// How To
 
 	var option = new UI.Row();
 	option.setClass( 'option' );
-	option.setTextContent( 'How To' );
+	option.setTextContent( 'How To Use' );
 	option.onClick( function () {
 
         document.getElementById('readme').style.display = "block";
@@ -43,6 +43,44 @@ Menubar.Help = function ( editor ) {
 
 	} );
 	options.add( option );
+
+    // Downloads
+
+    var option = new UI.Row();
+    option.setClass( 'option' );
+    option.setTextContent( 'Download Models' );
+    option.onClick( function () {
+
+        window.open( 'https://github.com/TheVindicators/Vis/tree/master/misc/models' );
+
+    } );
+    options.add( option );
+    options.add( new UI.HorizontalRule() );
+
+
+    // Report Issues
+
+    var option = new UI.Row();
+    option.setClass( 'option' );
+    option.setTextContent( 'Report Issues' );
+    option.onClick( function () {
+
+        window.open( 'https://github.com/TheVindicators/Vis/issues' );
+
+    } );
+    options.add( option );
+
+    // Feedback
+
+    var option = new UI.Row();
+    option.setClass( 'option' );
+    option.setTextContent( 'Leave Feedback' );
+    option.onClick( function () {
+
+        window.open( 'https://docs.google.com/forms/d/e/1FAIpQLSd-ruWSfoWmraXibXSOOzVwI7VNOERSUqEyeJ8dPQ_XdbOv8g/viewform' );
+
+    } );
+    options.add( option );
 
 	return container;
 
