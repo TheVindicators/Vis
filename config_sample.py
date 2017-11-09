0bd0f324a9dd18a123d6e3ed1e350efc1a645fc4
Christian Evans <cade9522@gmail.com>
2017-11-09 10:49:38 -0500

    re-add config_sample

---
+++ config_sample.py
@@ -0,0 +1,37 @@
+import os, subprocess
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY ='CHANGE THIS' #optional, since we may not be implementing authentication
+    try:
+        VERSION = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+        BRANCH = subprocess.check_output(['git', 'rev-parse', '--symbolic-full-name', '--abbrev-ref', 'HEAD'])
+    except:
+        VERSION = "Git not installed"
+        BRANCH = "Git not installed"
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    JSON_STORE_DATA = basedir + "/app/json_save_states/"
+    DEBUG = True
+    MODE = 'Development'
+
+class ProductionConfig(Config):
+    JSON_STORE_DATA = basedir + "/app/json_save_states/"
+    MODE ='Production'
+
+class TestingConfig(Config):
+    JSON_STORE_DATA = basedir + "/app/json_save_states/"
+    MODE = 'Testing'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
+}
