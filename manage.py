d5f12cf95492ad8b13ac143380892aceff346b16
David Lohle <Proplex@users.noreply.github.com>
2017-11-29 16:05:00 -0500

    Add server-side unittest, fix some bugs as a result of testing, harden endpoints

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ def test():
     unittest.TextTestRunner(verbosity = 2).run(tests)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command("runserver",Server(host="127.0.0.1",port="5000", threaded=True))
+manager.add_command("runserver",Server(host='127.0.0.1', port=5000, threaded=True))
 
 if __name__ == '__main__':
     manager.run()
